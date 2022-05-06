c8073b1c5c7f325d9996679ccef314fafc8380c6
Ardhi sasongko <ardhisasongko71@gmail.com>
2022-05-06 15:44:53 +0700

    Latihan pagination ke 3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { useState } from "react";
-import jsonData from "./MOCK_DATA.json";
+import JsonFake from "./MOCK_DATA.json";
 import ReactPaginate from "react-paginate";
+
 export default function App() {
-  const [users, setUser] = useState(jsonData.slice(0, 100));
+  const [users, setUsers] = useState(JsonFake.slice(0, 50));
   const [pageNumber, setPageNumber] = useState(0);
-  const userPerpage = 20;
+  const userPerpage = 10;
   const pagesVisited = pageNumber * userPerpage;
-  // page pagesVisited = 10 + (pagesVisited = 10 + pagesVisited + userPerpage = 10 => 20
+  // explanation displayusers
+  // let imagine we are in 30 and want to show 30- 40
+  // for item 30 is is a pagesVisited beacuse it has result userPerpage * pageNumber
+  // for item 40 it's result from pagesVisited = 30 + userPerpage = 10; => 40
   const displayUsers = users.slice(pagesVisited, pagesVisited + userPerpage).map((user) => {
     return (
       <div className="user">
         <h3>{user.first_name}</h3>
-        <h3>{user.first_lastname}</h3>
+        <h3>{user.last_name}</h3>
         <h3>{user.email}</h3>
       </div>
     );
