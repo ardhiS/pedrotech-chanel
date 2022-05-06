01c2d656a22e444f19ce71e0879d3d92f10a0bd3
Ardhi sasongko <ardhisasongko71@gmail.com>
2022-05-07 03:07:22 +0700

    memperbaharui file untuk latihan dalam menulis kode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { useState } from "react";
-import JsonFake from "./MOCK_DATA.json";
+import data from "./MOCK_DATA.json";
 import ReactPaginate from "react-paginate";
 
 export default function App() {
-  const [users, setUsers] = useState(JsonFake.slice(0, 50));
-  const [pageNumber, setPageNumber] = useState(0);
+  const [users, setUsers] = useState(data.slice(0, 50));
+  const [pageNumber, setPagenumber] = useState(0);
+
   const userPerpage = 10;
-  const pagesVisited = pageNumber * userPerpage;
-  // explanation displayusers
-  // let imagine we are in 30 and want to show 30- 40
-  // for item 30 is is a pagesVisited beacuse it has result userPerpage * pageNumber
-  // for item 40 it's result from pagesVisited = 30 + userPerpage = 10; => 40
-  const displayUsers = users.slice(pagesVisited, pagesVisited + userPerpage).map((user) => {
+  const userPerpageNumber = pageNumber * userPerpage;
+
+  const displayUser = users.slice(userPerpageNumber, userPerpageNumber + userPerpage).map((user) => {
     return (
       <div className="user">
         <h3>{user.first_name}</h3>
@@ -21,22 +19,21 @@ export default function App() {
       </div>
     );
   });
-
   const pageCount = Math.ceil(users.length / userPerpage);
   const changePage = ({ selected }) => {
-    setPageNumber(selected);
+    setPagenumber(selected);
   };
   return (
     <div className="App">
-      {displayUsers}
+      {displayUser}
       <ReactPaginate
-        previousLabel={"Previous"}
-        nextLabel={"Next"}
+        previousLabel={"<"}
+        nextLabel={">"}
         pageCount={pageCount}
         onPageChange={changePage}
         containerClassName={"paginationBttns"}
         previousLinkClassName={"previousBttn"}
-        nextLinkClassName={"nextBttn"}
+        nextLinkClassName={"nextBtn"}
         disabledClassName={"paginationDisabled"}
         activeClassName={"paginationActive"}
       />
